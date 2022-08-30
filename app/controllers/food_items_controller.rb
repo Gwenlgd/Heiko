29d3736c1544d4da60edf5ec409de56f6e9b1484
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <leg.gwenaelle@gmail.com>
2022-08-30 17:30:57 +0800

    Init: controllers

---
+++ app/controllers/food_items_controller.rb
@@ -0,0 +1,16 @@
+class FoodItemsController < ApplicationController
+  before_action :set_food_item, only: [:show]
+
+  def index
+    @food_items = FoodItem.all
+  end
+
+  def show
+  end
+
+  private
+
+  def set_listing
+    @food_item = FoodItem.find(params[:id])
+  end
+end
---
+++ app/controllers/report_moods_controller.rb
@@ -0,0 +1,2 @@
+class ReportMoodsController < ApplicationController
+end

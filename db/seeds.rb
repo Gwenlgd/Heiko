ed85df2f9615dfee6de5c7db7d9f027b80d87090
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <leg.gwenaelle@gmail.com>
2022-08-30 09:23:03 +0800

    added scss in home html

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+require "open-uri"
+
+puts "Cleaning database..."
+Rental.destroy_all
+
+
+User.destroy_all
+
+puts "Creating users..."

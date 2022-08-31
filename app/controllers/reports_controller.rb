260f6fb3669a9e2b01aef6a71e8b23c39fdbbef1
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <leg.gwenaelle@gmail.com>
2022-08-31 13:13:10 +0800

    Init: components, routes, add seeds

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,2 +1,16 @@
 class ReportsController < ApplicationController
+  before_action :set_report, only: [:show]
+
+  def index
+    @reports = Report.all
+  end
+
+  def show
+  end
+
+  private
+
+  def set_report
+    @report = Report.find(params[:id])
+  end
 end

3d9c400f8ded02b01e194af134b1278678f7398d
Jules Buckley <juliettebuckley@icloud.com>
2022-09-05 09:56:42 +0800

    cancel account stuff

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,8 +1,8 @@
 class Report < ApplicationRecord
   belongs_to :user
-  has_many :report_feelings
-  has_many :report_moods
-  has_many :report_food_items
+  has_many :report_feelings, dependent: :destroy
+  has_many :report_moods, dependent: :destroy
+  has_many :report_food_items, dependent: :destroy
 
 
 end
